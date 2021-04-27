95a5c9e409c77120e17cf360cd4508f7b3e16699
Jimmie-S <jimmie@it-proffs.net>
2021-04-27 11:13:24 +0200

    New files

---
+++ Profile/Powershell-Profile.ps1
@@ -0,0 +1,31 @@
+﻿
+Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
+
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+Install-Module -Name 'posh-git'
+
+
+# Set your PowerShell execution policy
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+
+# Install Chocolatey
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+
+# Install Chocolatey packages
+choco install git.install -y
+choco install conemu -y
+
+# Install PowerShell modules
+Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name 'posh-git'
+
+# Creates profile if doesn't exist then edits it
+
+if (!(Test-Path -Path $PROFILE)){ New-Item -Path $PROFILE -ItemType File } ; powershell_ise.exe $PROFILE
+
+
+Import-Module -Name posh-git
+$StartSshAgent = "Start-Service ssh-agent"
+$StartSshAgent
