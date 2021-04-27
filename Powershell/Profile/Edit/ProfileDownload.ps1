0e7613224c4b023b01c3e1b7fb1f884d7ff8938e
Jimmie-S <Jimmie@it-proffs.net>
2021-04-27 22:55:30 +0200

    Test

---
+++ Powershell/Profile/Edit/ProfileDownload.ps1
@@ -0,0 +1,10 @@
+#URL to profile script
+$URI = "https://git.itgarden.se/projects/OR/repos/openrepo/raw/Powershell/Powershell%20Profiles/Microsoft.PowerShell_profile.ps1"
+
+#Create/Overwrite current profile
+New-Item -Path $PROFILE -ItemType file -Force
+
+#Download Powershell Profile
+Invoke-WebRequest $URI | Select-Object -ExpandProperty Content | Out-file -FilePath "$PROFILE" -Force
+
+Test
