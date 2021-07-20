dae31bb3813cfc756e266959af951ecac929efd4
Jimmie-S <Jimmie@it-proffs.net>
2021-07-20 08:33:41 +0200

    Test

--- Exchange Powershell/PowershellExchange-Master.ps1
+++
@@ -1,8 +0,0 @@
-
-############################## -  Load Powershell SnapIn
-
-Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
-
-
-$alias = "Alias"
-New-Mailbox -Name $alias -Alias $alias -OrganizationalUnit "OU=Shared,OU=Funktionskonton,DC=resurs,DC=loc" -Database "MBXB002" -UserPrincipalName "$alias@resurs.se" -Shared
