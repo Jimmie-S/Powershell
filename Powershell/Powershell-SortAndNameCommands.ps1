9c7c472d67cab08198ffb0bb8a992c91b9ca39a2
Jimmie-S <Jimmie@it-proffs.net>
2021-07-22 13:29:43 +0200

    Moved and updated

---
+++ Powershell/Powershell-SortAndNameCommands.ps1
@@ -0,0 +1,19 @@
+$DateCutOff = (Get-Date).AddDays(-30)
+Get-ADUser -Filter * -Properties whenCreated | Where-Object {$_.whenCreated -gt $DateCutOff} | Format-Table Name, whenCreated
+
+
+
+Install-Script -Name SCONFIG
+
+
+Import-Csv "C:\Path\To.csv" | ForEach-Object {
+    Get-ADUser -Filter "DisplayName -eq '$($_.DisplayName)'" -Properties SamAccountName |
+ Select-Object SamAccountName
+ } | Export-CSV C:\Path\To.SamAccountName.csv -NoTypeInformation
+ 
+  $file = Import-Csv "C:\Temp\SamAccountName.csv"
+ foreach ($line in $file){
+     Add-ADGroupMember -Identity "ADGroup" -Members $line
+     }
+ 
+ 
